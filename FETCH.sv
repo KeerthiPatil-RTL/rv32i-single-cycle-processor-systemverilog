3d49bef80dfb29fb2d203336ba103a35f337e7d2
Keerthi Patil <keerthipatil2001@gmail.com>
2026-02-14 16:23:42 -0500

    Fix formatting and add endmodule to FETCH.sv

--- FETCH.sv
+++ FETCH.sv
@@ -3,7 +3,7 @@ module fetch ( input logic [31:0] pc,
                input logic [31:0]imem_data,
                output logic [31:0] imem_addr,
                output logic [31:0] instruction,
-               output logic imem_req);
+               output logic imem_req);  
 
 logic req_reg;
 
@@ -17,3 +17,4 @@ end
 assign imem_addr = pc;
 assign req_reg = imem_req;
 assign instruction = imem_data;
+endmodule
